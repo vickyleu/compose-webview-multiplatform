e11b5ce5611dac3e97b1a57fdc518bc9ccb49ab0
vicky leu <yu6564172@gmail.com>
2024-05-14 14:20:08 +0800

    adding prompt and navigation interceptor

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ plugins {
     alias(libs.plugins.android.application).apply(false)
     alias(libs.plugins.android.library).apply(false)
     alias(libs.plugins.jetbrains.compose).apply(false)
+    alias(libs.plugins.compose.compiler).apply(false)
     alias(libs.plugins.dokka)
     alias(libs.plugins.ktlint)
     alias(libs.plugins.kotlin.plugin.atomicfu)
