19363288e13af1c73d763518c628d146dcee7a32
vicky leu <yu6564172@gmail.com>
2024-09-30 02:51:14 +0800

    1.7.0 beta02

--- webview/src/commonMain/kotlin/com/multiplatform/webview/jsbridge/JsMessageDispatcher.kt
+++ webview/src/commonMain/kotlin/com/multiplatform/webview/jsbridge/JsMessageDispatcher.kt
@@ -37,18 +37,59 @@ internal class JsMessageDispatcher {
         jsHandlerMap.clear()
     }
 
+//    fun postWebviewDelegateMethod(webView: IWebView?, jsBridgeName: String) {
+//        jsHandlerMap.forEach { (key, value) ->
+//            webView?.evaluateJavaScript(
+//                """
+//            window.$jsBridgeName.${key} = function (...args) {
+//                function isJsonString(str) {
+//                     try {
+//                         JSON.parse(str);
+//                     } catch (e) {
+//                         return false;
+//                     }
+//                     return true;
+//                }
+//                if (args.length >= ${value.minimalParamCount()} && args.length <= ${value.methodParamCount()}) {
+//                    let params;
+//                    if (args.length === 1) {
+//                        params = isJsonString(args[0]) ? args[0] : JSON.stringify(args[0]);
+//                    } else {
+//                        params = JSON.stringify(args.reduce((acc, arg, index) => {
+//                            acc[`key${'$'}{index}`] = arg;
+//                            return acc;
+//                        }, {}));
+//                    }
+//                    ${(if (value.isCallbackMethod()) {
+//                        """return new Promise(  (resolve, reject) => {
+//                                window.$jsBridgeName.callNative('$key', params, function (res) {
+//                                    resolve(res);  // 将返回值通过 resolve 返回
+//                                });
+//                           }
+//                        );""".trimIndent()
+//                    } else {
+//                        """window.$jsBridgeName.callNative('$key', params);""".trimIndent()
+//                    })}
+//                    } else {
+//                        console.error('Invalid number of arguments for ${key}');
+//                    }
+//            };""".trimIndent()
+//            )
+//        }
+//    }
+
     fun postWebviewDelegateMethod(webView: IWebView?, jsBridgeName: String) {
         jsHandlerMap.forEach { (key, value) ->
             webView?.evaluateJavaScript(
                 """
             window.$jsBridgeName.${key} = function (...args) {
                 function isJsonString(str) {
-                     try {
-                         JSON.parse(str);
-                     } catch (e) {
-                         return false;
-                     }
-                     return true;
+                    try {
+                        JSON.parse(str);
+                    } catch (e) {
+                        return false;
+                    }
+                    return true;
                 }
                 if (args.length >= ${value.minimalParamCount()} && args.length <= ${value.methodParamCount()}) {
                     let params;
@@ -56,28 +97,27 @@ internal class JsMessageDispatcher {
                         params = isJsonString(args[0]) ? args[0] : JSON.stringify(args[0]);
                     } else {
                         params = JSON.stringify(args.reduce((acc, arg, index) => {
-                            acc[`key${'$'}{index}`] = arg;
+                            acc['key' + index] = arg;  // 使用 'key' + index
                             return acc;
                         }, {}));
                     }
                     ${
-                    if (value.isCallbackMethod()) {
-                        """return new Promise((resolve, reject) => {
-                                window.$jsBridgeName.callNative('$key', params, function (res) {
-                                    resolve(res);  // 将返回值通过 resolve 返回
+                        if (value.isCallbackMethod()) {
+                            """return new Promise((resolve, reject) => {
+                                    window.$jsBridgeName.callNative('$key', params, function (res) {
+                                        resolve(res);  // 将返回值通过 resolve 返回
+                                    });
                                 });
-                            });
-                        });""".trimIndent()
-                    } else {
-                        """window.$jsBridgeName.callNative('$key', params);""".trimIndent()
+                            """.trimIndent()
+                        } else {
+                            """window.$jsBridgeName.callNative('$key', params);""".trimIndent()
+                        }
                     }
-                }
                 } else {
                     console.error('Invalid number of arguments for ${key}');
                 }
             };
-        """.trimIndent()
-            )
+            """.trimIndent())
         }
     }
 }
