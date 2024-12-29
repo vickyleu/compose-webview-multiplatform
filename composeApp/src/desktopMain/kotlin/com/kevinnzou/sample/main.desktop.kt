3f385c9af8db714ba59b544b8a6f55a1dcdf0412
vickyleu <yu6564172@gmail.com>
2024-12-29 11:44:04 +0800

    remove KT-51379 workaround

--- composeApp/src/desktopMain/kotlin/com/kevinnzou/sample/main.desktop.kt
+++ composeApp/src/desktopMain/kotlin/com/kevinnzou/sample/main.desktop.kt
@@ -1,9 +1,70 @@
 package com.kevinnzou.sample
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import dev.datlag.kcef.KCEF
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.File
 
 
+fun main() =
+    application {
+        Window(onCloseRequest = ::exitApplication) {
+            var restartRequired by remember { mutableStateOf(false) }
+            var downloading by remember { mutableStateOf(0F) }
+            var initialized by remember { mutableStateOf(false) }
+
+            LaunchedEffect(Unit) {
+                withContext(Dispatchers.IO) {
+                    KCEF.init(builder = {
+                        installDir(File("kcef-bundle"))
+                        progress {
+                            onDownloading {
+                                downloading = it
+                            }
+                            onInitialized {
+                                initialized = true
+                            }
+                        }
+                        release("jbr-release-17.0.10b1087.23")
+                        settings {
+                            cachePath = File("cache").absolutePath
+                        }
+                    }, onError = {
+                        it?.printStackTrace()
+                    }, onRestartRequired = {
+                        restartRequired = true
+                    })
+                }
+            }
+
+            if (restartRequired) {
+                Text(text = "Restart required.")
+            } else {
+                if (initialized) {
+                    MainWebView()
+                } else {
+                    Text(text = "Downloading $downloading%")
+                }
+            }
+
+            DisposableEffect(Unit) {
+                onDispose {
+                    KCEF.disposeBlocking()
+                }
+            }
+        }
+    }
 
 @Composable
 fun MainWebView() = WebViewApp()
--- webview/src/desktopMain/kotlin/com/multiplatform/webview/web/WebView.desktop.kt
+++ webview/src/desktopMain/kotlin/com/multiplatform/webview/web/WebView.desktop.kt
@@ -4,13 +4,13 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.awt.SwingPanel
 import com.multiplatform.webview.jsbridge.WebViewJsBridge
-import compose_webview_multiplatform.webview.generated.resources.Res
 import dev.datlag.kcef.KCEF
 import dev.datlag.kcef.KCEFBrowser
 import dev.datlag.kcef.KCEFClient
 import org.cef.browser.CefRendering
 import org.cef.browser.CefRequestContext
 import org.jetbrains.compose.resources.ExperimentalResourceApi
+import webview.generated.resources.Res
 
 /**
  * Desktop WebView implementation.
