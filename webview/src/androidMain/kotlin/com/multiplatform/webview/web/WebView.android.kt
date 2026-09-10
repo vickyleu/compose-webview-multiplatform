86a11ad6974641f58644cb90d2921418b54b6462
VickyLeu <yu6564172@gmail.com>
2026-09-10 00:00:14 -0700

    merge: add Android console bridge and platform WebView params

--- webview/src/androidMain/kotlin/com/multiplatform/webview/web/WebView.android.kt
+++ webview/src/androidMain/kotlin/com/multiplatform/webview/web/WebView.android.kt
@@ -2,13 +2,13 @@ package com.multiplatform.webview.web
 
 import android.content.Context
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import com.multiplatform.webview.jsbridge.ConsoleBridge
 import com.multiplatform.webview.jsbridge.WebViewJsBridge
 
-
-/**
- * Android WebView implementation.
- */
+/** Android WebView implementation. */
 @Composable
 actual fun ActualWebView(
     state: WebViewState,
@@ -16,24 +16,36 @@ actual fun ActualWebView(
     captureBackPresses: Boolean,
     navigator: WebViewNavigator,
     webViewJsBridge: WebViewJsBridge?,
+    consoleBridge: ConsoleBridge?,
     onCreated: (NativeWebView) -> Unit,
     onDispose: (NativeWebView) -> Unit,
+    platformWebViewParams: PlatformWebViewParams?,
     factory: (WebViewFactoryParam) -> NativeWebView,
 ) {
     AccompanistWebView(
-        state,
-        modifier,
-        captureBackPresses,
-        navigator,
-        webViewJsBridge,
+        state = state,
+        modifier = modifier,
+        captureBackPresses = captureBackPresses,
+        navigator = navigator,
+        webViewJsBridge = webViewJsBridge,
+        consoleBridge = consoleBridge,
         onCreated = onCreated,
         onDispose = onDispose,
+        client = platformWebViewParams?.client ?: remember { AccompanistWebViewClient() },
+        chromeClient =
+            platformWebViewParams?.chromeClient ?: remember { AccompanistWebChromeClient() },
         factory = { factory(WebViewFactoryParam(it)) },
     )
 }
 
-/** Android WebView factory parameters: a context. */
-actual data class WebViewFactoryParam(val context: Context)
+actual data class WebViewFactoryParam(
+    val context: Context,
+)
 
-/** Default WebView factory for Android. */
 actual fun defaultWebViewFactory(param: WebViewFactoryParam) = android.webkit.WebView(param.context)
+
+@Immutable
+actual data class PlatformWebViewParams(
+    val client: AccompanistWebViewClient? = null,
+    val chromeClient: AccompanistWebChromeClient? = null,
+)
