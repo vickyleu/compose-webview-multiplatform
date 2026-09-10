0684bdb64ccf95c55d12a23d0e770ee57a12634b
VickyLeu <yu6564172@gmail.com>
2026-09-10 04:44:56 -0700

    fix(wasm): make bridge bootstrap idempotent

--- webview/src/wasmJsMain/kotlin/com/multiplatform/webview/web/WebViewJsBridge.kt
+++ webview/src/wasmJsMain/kotlin/com/multiplatform/webview/web/WebViewJsBridge.kt
@@ -6,48 +6,51 @@ internal fun createJsBridgeScript(
 ): String {
     val bridgeObjectCode =
         """
-        window.$jsBridgeName = {
-            _callbacks: {},
-            _callbackId: 0,
-            postMessage: function(methodName, params, callbackId) {
-                var messageData = JSON.stringify({
-                    type: '$jsBridgeName',
-                    action: methodName,
-                    params: params,
-                    callbackId: callbackId || 0
-                });
-                parent.postMessage(messageData, '*');
-            },
-            onCallback: function(callbackId, message) {
-                var callback = this._callbacks[callbackId];
-                if (callback) {
-                    callback(message);
-                    delete this._callbacks[callbackId];
+        if (!window.$jsBridgeName || window.$jsBridgeName.__kmpWasmBridge !== true) {
+            window.$jsBridgeName = {
+                __kmpWasmBridge: true,
+                _callbacks: {},
+                _callbackId: 0,
+                postMessage: function(methodName, params, callbackId) {
+                    var messageData = JSON.stringify({
+                        type: '$jsBridgeName',
+                        action: methodName,
+                        params: params,
+                        callbackId: callbackId || 0
+                    });
+                    parent.postMessage(messageData, '*');
+                },
+                onCallback: function(callbackId, message) {
+                    var callback = this._callbacks[callbackId];
+                    if (callback) {
+                        callback(message);
+                        delete this._callbacks[callbackId];
+                    }
+                },
+                call: function(action, params, callback) {
+                    var callbackId = 0;
+                    if (callback) {
+                        callbackId = ++this._callbackId;
+                        this._callbacks[callbackId] = callback;
+                    }
+                    this.postMessage(action, params, callbackId);
+                    return callbackId;
+                },
+                callNative: function(methodName, params, callback) {
+                    return this.call(methodName, params, callback);
                 }
-            },
-            call: function(action, params, callback) {
-                var callbackId = 0;
-                if (callback) {
-                    callbackId = ++this._callbackId;
-                    this._callbacks[callbackId] = callback;
+            };
+            window.addEventListener('message', function(event) {
+                try {
+                    var data = typeof event.data === 'string' ? JSON.parse(event.data) : event.data;
+                    if (data && data.type === '$jsBridgeName') {
+                        window.$jsBridgeName.onCallback(data.callbackId, data.message);
+                    }
+                } catch (e) {
+                    console.error('Error processing callback message:', e);
                 }
-                this.postMessage(action, params, callbackId);
-                return callbackId;
-            },
-            callNative: function(methodName, params, callback) {
-                return this.call(methodName, params, callback);
-            }
-        };
-        window.addEventListener('message', function(event) {
-            try {
-                var data = typeof event.data === 'string' ? JSON.parse(event.data) : event.data;
-                if (data && data.type === '$jsBridgeName') {
-                    window.$jsBridgeName.onCallback(data.callbackId, data.message);
-                }
-            } catch (e) {
-                console.error('Error processing callback message:', e);
-            }
-        });
+            });
+        }
         """.trimIndent()
 
     return if (isIife) {
